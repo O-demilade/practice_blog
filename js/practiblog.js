7ad6bbdd05008b81d8519ebd00e380217de9df1c
Demi <demmypek1@gmao.com>
2021-08-03 01:16:20 +0100

    overwriting effect canceled

--- js/practiblog.js
+++ js/practiblog.js
@@ -2,7 +2,10 @@
  * Created by Demi on 15-Jul-21.
  */
 window.addEventListener("DOMContentLoaded", function(d){
+  const dat = Date();
+
   console.log("loaded");
+  console.log(Date());
 
   // select all the buttons that pertains to adding a post
   const addBtn = document.querySelector("header>button");
@@ -10,16 +13,27 @@ window.addEventListener("DOMContentLoaded", function(d){
   const postBtn = document.querySelector(".post");
   
   const postsTitles = [];
+  let display = "";
 
+  // assign the elements for the articles display 
   const outTitle = document.querySelector(".title");
   const outWriteup = document.querySelector(".writeup");
   const outAuthor = document.querySelector(".author");
 
+  // get all the blog post titles from Storage
+  const postTitles = localStorage.getItem("PractiBlogTitles");
+  let postTitlesParse = JSON.parse(postTitles);
+
+  // select the last post and display it 
+  if (postTitlesParse != null){
+    display = postTitlesParse[(postTitlesParse["length"]-1)];
+  };
+
   // check storage (localStorage) for PractiBlog post
-  const outPost = localStorage.getItem("PractiBlogPost");
+  const outPost = localStorage.getItem(display);
 
   // convert the Storage data back from a string
-  outTemp = JSON.parse(outPost);
+  let outTemp = JSON.parse(outPost);
 
   if (outPost && outTemp.title != "" && outTemp.writeup != "") {
 
@@ -45,9 +59,15 @@ window.addEventListener("DOMContentLoaded", function(d){
       writeup:postCont.querySelector("#post_writeup").value,
       author:postCont.querySelector("#post_author").value
     };
+
+    // assign the post title to the variable title 
+    let title = postCont.querySelector("#post_title").value;
+
+    // add the title to the list of titles 
+    postsTitles.push(title);
     
     // store the post data as a string in browser memory
-    localStorage.setItem("PractiBlogPost", JSON.stringify(post));
-
+    localStorage.setItem(title, JSON.stringify(post));
+    localStorage.setItem("PractiBlogTitles", JSON.stringify(postsTitles))
   });
 });
