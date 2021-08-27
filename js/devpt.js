3e08e94bb415476ad3d312a2146b5dbaf82698da
Demi <demmypek1@gmao.com>
2021-08-27 13:13:38 +0100

    Something that works

--- js/devpt.js
+++ js/devpt.js
@@ -1,20 +1,19 @@
-"use strict";
 
-function devptDisplay(display){
-  let screen = document.querySelector("#devpt-display");
+// function devptDisplay(display){
+//   let screen = document.querySelector("#devpt-display");
   
   
-  if (display !== (null, "") && display){
-    screen.style.display = "block";
-    screen.textContent = display;
-  };
-};
+//   if (display !== (null, "") && display){
+//     screen.style.display = "block";
+//     screen.textContent = display;
+//   };
+// };
 
-const storage = {
-  image: window.localStorage,
-  len: window.localStorage.length,
-  titles: window.localStorage.getItem("practiBlogTitles")
-};
+// const storage = {
+//   image: window.localStorage,
+//   len: window.localStorage.length,
+//   titles: window.localStorage.getItem("practiBlogTitles")
+// };
 
 // devptDisplay(storage.titles);
 // devptDisplay(global.postLinks);
--- js/practiblog.js
+++ js/practiblog.js
@@ -2,139 +2,89 @@
  * Created by Demi on 15-Jul-21.
  */
 "use strict";
+
 const global ={
   // select all the buttons that pertains to adding a post
   addBtn : document.querySelector("header>button"),
   clsBtn : document.querySelector("#cls_add"),
   postBtn : document.querySelector(".post"),
-
-  // assign the elements for the articles display 
-  outTitle : document.querySelector(".title"),
-  outWriteup : document.querySelector(".writeup"),
-  outAuthor : document.querySelector(".author"),
-  editButn : document.querySelector("#edit"),
-    
-  postTitles : [],
-  display : "",
-
-  postLinks : document.querySelector(".titles>a")
 };
 
-window.addEventListener("DOMContentLoaded", function(d){
+window.addEventListener("DOMContentLoaded", function(){  
+  let practiBlogTitlesStores = JSON.parse(localStorage.getItem("practiBlogTitles"));
 
-  console.log("loaded");
+  //initialize the practiblog titles local storage
+  if(localStorage.getItem("practiBlogTitles")){
+    // do nothing
+  }
+  else{
+    let practiBlogTitlesStore = [];
+    window.localStorage.setItem("practiBlogTitles", JSON.stringify(practiBlogTitlesStore))};
+
+  //populate the posts display
+  let artCount;
+  if(practiBlogTitlesStores.length >= 5){artCount = 5}else{artCount = practiBlogTitlesStores.length};
+  if(artCount>0){document.querySelector(".output").innerHTML = ""}
+  while (artCount > 0) {
+    document.querySelector(".output").innerHTML += addDisplay(practiBlogTitlesStores[artCount-1]);
+    artCount -= 1;
+  }
   
+  //populate the list of titles *move this to the bottom of the listener to prevent initial sorting
+  practiBlogTitlesStores.sort().forEach(element => {
+    document.querySelector(".titles").innerHTML += `<a href="">${element}</a>`;
+  });
+  console.info("DOM content loaded" );
+}
+);
 
-  function populatePostTitles(){
-    // get all the blog post titles from Storage
-    if (window.localStorage.getItem("practiBlogTitles") === null){
-      window.localStorage.setItem("practiBlogTitles", JSON.stringify(global.postTitles));
-    }
-    else {
-      // get list of all titles available in storage
-      global.postTitles = (JSON.parse(window.localStorage.getItem("practiBlogTitles")));
-    }
-    // assign each post title a link on the Posts pane
-    global.postTitles.sort();
-    global.postTitles.forEach(
-      function log(item){
-        let link = (`<a href="">${item}</a>`);
-        document.querySelector(".titles").innerHTML += link;
-      }
-    );
-  };
-  
-  populatePostTitles();
-  // // select the last post and display it 
-  if (global.postTitles.length !== 0){
-    global.display = global.postTitles[(global.postTitles["length"]-1)];
-    //  addDisplay(global.display);
-  };
+global.addBtn.addEventListener("click", function(d){
+  document.querySelector(".input").style.display="block";
+});
 
-  const outPost = localStorage.getItem(global.display);
+global.clsBtn.addEventListener("click", function(d){
+  document.querySelector(".input").style.display="none";
+});
+
+global.postBtn.addEventListener("click", function(d){
+  const postCont = d.currentTarget.parentNode;
+  const post = {
+    title:postCont.querySelector("#post_title").value,
+    writeup:postCont.querySelector("#post_writeup").value,
+    author:postCont.querySelector("#post_author").value,
+    timeStamp: new Date,
+  };
+  // console.log(global.practiBlogTitles);
+  // Add post title to titles list
+  let practiBlogTitlesStores = JSON.parse(localStorage.getItem("practiBlogTitles"));
+  practiBlogTitlesStores.push(post.title);
+  console.log(practiBlogTitlesStores);
+  // store the post data as a string in browser memory
+  localStorage.setItem(post.title, JSON.stringify(post));
+  localStorage.setItem("practiBlogTitles", JSON.stringify(practiBlogTitlesStores));
+});
+
+function addDisplay(title){
+  let outPost = localStorage.getItem(title);
 
   //.. convert the Storage data back from a string
   let outTemp = JSON.parse(outPost);
+  let title_ = "";
+  let writeup_ = "";
+  let author_ = "";
+  let display_ = "";
 
   if (outPost && outTemp.title !== "" && outTemp.writeup !== "") {
-
-    global.outTitle.innerText = outTemp.title;
-    global.outWriteup.innerText = outTemp.writeup;
-    global.outAuthor.innerText = "- " + outTemp.author;
-    global.editButn.style.display = "block";
+    title_ = outTemp.title;
+    writeup_ = outTemp.writeup;
+    author_ = "- " + outTemp.author;
+    display_ = "block";
   };
-
-  // function addDisplay(title){
-  //   let outPost = localStorage.getItem(title);
-  
-  //   //.. convert the Storage data back from a string
-  //   let outTemp = JSON.parse(outPost);
-  //   let title_ = "";
-  //   let writeup_ = "";
-  //   let author_ = "";
-  //   let display_ = "";
-  
-  //   if (outPost && outTemp.title !== "" && outTemp.writeup !== "") {
-  //     title_ = outTemp.title;
-  //     writeup_ = outTemp.writeup;
-  //     author_ = "- " + outTemp.author;
-  //     display_ = "block";
-  //   };
-  //   return(
-  //     `<div class="output">
-  //       <h1 class="title">${title_}</h1>
-  //       <div class="writeup">${writeup_}</div>
-  //       <h3 class="author">${author_}</h3>
-  //       <button id="edit" style="display: ${display_};">edit post</button>
-  //   </div>`);
-  // };
-
-  // if(global.postTitles.length >= 5){
-  //   devptDisplay("> 5");
-  //   for(let i=1; i<6; i++){
-  //     document.querySelector(".display").innerHTML += addDisplay(global.postTitles[(global.postTitles["length"]-i)]);
-  //   };};
-    
-  // if(global.postTitles.length < 5){
-  //   devptDisplay("> 5");
-  //   for(let i=1; i<(global.postTitles["length"]+1); i++){
-  //     devptDisplay(i);
-  //     document.querySelector(".display").innerHTML += addDisplay(global.postTitles[(global.postTitles["length"]-i)]);
-  //   };};
-  // }else{
-  //   devptDisplay("< 5" + global.postTitles["length"]);
-    // for(let i=1; i<global.postTitles["length"]; i++){
-    //   document.querySelector(".display").innerHTML += addDisplay(global.postTitles[(global.postTitles["length"]-i)]);
-    // };
-  // };
-  /*
-  * * * * * * * * * * * * * * * * * * *
-  * * *  Buttons actions section  * * *
-  * * * * * * * * * * * * * * * * * * * 
-  */
-  global.addBtn.addEventListener("click", function(d){
-    document.querySelector(".input").style.display="block";
-  });
-
-  global.clsBtn.addEventListener("click", function(d){
-    document.querySelector(".input").style.display="none";
-  });
-
-  global.postBtn.addEventListener("click", function(d){
-    
-    const postCont = d.currentTarget.parentNode;
-    const post = {
-      title:postCont.querySelector("#post_title").value,
-      writeup:postCont.querySelector("#post_writeup").value,
-      author:postCont.querySelector("#post_author").value,
-      timeStamp: new Date,
-    };
-
-    // Add post title to titles list
-    global.postTitles.push(post.title);
-
-    // store the post data as a string in browser memory
-    localStorage.setItem(post.title, JSON.stringify(post));
-    localStorage.setItem("practiBlogTitles", JSON.stringify(global.postTitles));
-  });
-});
+  return(
+    `<div class="output">
+      <h1 class="title">${title_}</h1>
+      <div class="writeup">${writeup_}</div>
+      <h3 class="author">${author_}</h3>
+      <button id="edit" style="display: ${display_};">edit post</button>
+  </div>`);
+};
